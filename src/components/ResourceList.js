e2c3169969ad68f56ad6be25ff76c86016b835b6
Mayank <gamblergamer9@gmail.com>
2020-10-03 20:25:14 +0530

    code cleanup

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -16,23 +16,6 @@ const useResources = (resource) => {
 }
 
 const ResourceList = ({ resource }) => {
-  // const [resources, setResources] = useState([]);
-
-  // // const fetchResource = async (resource) => {
-  // //   const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
-
-  // //   setResources(response.data);
-  // // }
-
-  // useEffect(() => {
-  //   // fetchResource(resource);
-  //   (async (resource) => {
-  //     const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
-
-  //     setResources(response.data);
-  //   })(resource);
-  // }, [resource]);
-
   const resources = useResources(resource);
 
   return (
